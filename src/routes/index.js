5865990f583c3b15499cc281c68d2eea7a7651ae
Rh-1023 <rokhwan1023@gmail.com>
2021-05-13 15:27:34 +0900

    Create project

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import { createRouter , createWebHashHistory} from 'vue-router'
+import Home from './Home'
+import About from './About'
+import Movie from './Movie'
+import NotFound from './NotFound'
+
+export default createRouter ({
+    //hash
+    history: createWebHashHistory(),
+    scrollBehavior(){
+        return { top: 0 }
+    },
+    // pages
+    routes : [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/movie/:id',
+            component: Movie
+        },
+        {
+            path: '/about',
+            component: About
+        },
+        {
+            path:'/:notFound(.*)',
+            component: NotFound
+        }
+    ]
+})
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default{
+    namespaced: true,
+    state: () => ({
+        name: 'ROKHWAN',
+        email: 'rokhwan1023@gmail.com',
+        blog: 'https://heropy.blog',
+        phone: '010-7608-4806',
+        image: 'https://heropy.blog/css/images/logo.png'
+    })
+}
